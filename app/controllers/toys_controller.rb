5ea8d3cda64a9bdcfbf6f519a38fb3ecd77cd414
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-06-25 15:00:19 +0300

    complete lab

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,18 +7,23 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    if toy.update(toy_params)
+      render json: toy
+    else
+      render json: { error: 'Failed to update toy' }, status: :unprocessable_entity
+    end
   end
+  
 
   def destroy
     toy = Toy.find_by(id: params[:id])
-    toy.destroy
+    toy.destroy!
     head :no_content
   end
 
